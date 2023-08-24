8b3ca16b10be2995b6360a9f4cd6bb0da7b4a619
Lillysmart <137174363+Lillysmart@users.noreply.github.com>
2023-08-24 11:26:49 +0200

    update Challenge 2

---
+++ Challenge 2/scipts.js
@@ -0,0 +1,48 @@
+
+function add ( a, b)  {return a + b }
+
+function multiply (a, b)   {return a * b }
+console.log (multiply(2,6))
+console.log (add (5,4))
+
+
+function internal(a,c) {
+	const added = add(1,2)
+	const multiplied =multiply(this,2,1)
+	return  [ multiplied]
+	
+	
+}
+
+// Not allowed to change below this
+
+const example1 = {
+	internal: {
+		a: 2,
+		b: 4,
+		c: 8,
+	},
+	add,
+	multiply,
+  calculate: internal
+}
+
+console.log (example1.calculate())
+
+
+const example2 = {
+	internal: {
+		a: 2,
+		b: 2,
+		c: 3,
+	},
+	add,
+	multiply,
+  calculate: internal
+}
+console.log (example2.calculate())
+
+
+
+example1.calculate()
+example2.calculate()
