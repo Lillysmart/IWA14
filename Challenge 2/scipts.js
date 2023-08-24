d50f7e3214d0392a74b1f948deb98a63b291e581
Lillysmart <137174363+Lillysmart@users.noreply.github.com>
2023-08-24 11:29:18 +0200

    Update scipts.js

--- Challenge 2/scipts.js
+++ Challenge 2/scipts.js
@@ -1,48 +1,42 @@
+function add(a, b) {
+  return a + b;
+}
 
-function add ( a, b)  {return a + b }
+function multiply(a, b) {
+  return a * b;
+}
+console.log(multiply(2, 6));
+console.log(add(5, 4));
 
-function multiply (a, b)   {return a * b }
-console.log (multiply(2,6))
-console.log (add (5,4))
-
-
-function internal(a,c) {
-	const added = add(1,2)
-	const multiplied =multiply(this,2,1)
-	return  [ multiplied]
-	
-	
+function internal(a, c) {
+  const added = add(1, 2);
+  const multiplied = multiply(this, 2, 1);
+  return [multiplied];
 }
 
 // Not allowed to change below this
 
 const example1 = {
-	internal: {
-		a: 2,
-		b: 4,
-		c: 8,
-	},
-	add,
-	multiply,
-  calculate: internal
-}
-
-console.log (example1.calculate())
-
+  internal: {
+    a: 2,
+    b: 4,
+    c: 8,
+  },
+  add,
+  multiply,
+  calculate: internal,
+};
 
 const example2 = {
-	internal: {
-		a: 2,
-		b: 2,
-		c: 3,
-	},
-	add,
-	multiply,
-  calculate: internal
-}
-console.log (example2.calculate())
+  internal: {
+    a: 2,
+    b: 2,
+    c: 3,
+  },
+  add,
+  multiply,
+  calculate: internal,
+};
 
-
-
-example1.calculate()
-example2.calculate()
+example1.calculate();
+example2.calculate();
